58f3a3655c333d74ced6789233e156e5f7645e11
Jordan <68711561+jmtyson10@users.noreply.github.com>
2022-12-30 16:00:19 -0700

    search bar
    worked on search bar. I have one that works but it does not filter correctly.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FEWD-Project-5
- 
+
+sass --watch scss/styles.scss:css/styles.css
+
+npm init
--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 baguetteBox.run('.gallery');
 
 const searchInput = document.querySelector('[data-search]')
-
-let cards = []
+let captions = document.getElementsByTagName('a')
 
 searchInput.addEventListener("input", e => {
-    const value = e.target.value.toLowerCase()
-    cards.forEach('[card]' => {
-        const isVisible = 
+    const value = e.target.value.toLowerCase();
+
+    for (i = 0; i< captions.length; i++) {
+        if (!captions[i].innerHTML.toLowerCase().includes(value)) {
+            captions[i].style.display="none";
+        } else {
+            captions[i].style.display="";
+        }}
     })
-})
